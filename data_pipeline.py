554cda511d29c5e9d3ec0acb7c9d06287ddf1f9b
Jerry Chu <jerrychu123@gmail.com>
2021-02-23 18:49:13 -0800

    :sparkles: Finishing up ticker filter in pipeline class
    Added new methods to filter pipeline tickers to ones that can be traded. Also updated test file naming structure

--- data_pipeline.py
+++ data_pipeline.py
@@ -19,7 +19,22 @@ class Robin_Pipeline(object):
     Class for interacting with the robinhood api to pull in ticker and other
     asset level information including historical data
     """
-    def check_ticker(self, tickers: list) -> dict or None:
+
+
+    def __init__(self, ticker_list: list, needs_fractional: bool) -> None:
+        self.tickers = ticker_list
+        self.needs_fractional = needs_fractional
+
+
+    def set_tickers(self, tickers: list) -> None:
+        """
+        method to mutate the object variable-tickers
+        """
+        self.tickers = tickers
+
+
+    @static_method
+    def check_ticker(tickers: list) -> dict or None:
         """
         method used to check if a list of tickers exists on robinhood and can actually
         be traded or not.
@@ -29,7 +44,7 @@ class Robin_Pipeline(object):
             tradability = {}
             for ticker_result in results:
                 tradability[ticker_result.get('symbol')] = {
-                    'tradeable': ticker_result.get('tradeable'),
+                    'tradable': ticker_result.get('tradable'),
                     'fractional_tradability': ticker_result.get('fractional_tradability')
                 }
             return tradability
@@ -39,3 +54,33 @@ class Robin_Pipeline(object):
                          Robinhood API not returning results for ticker query at
                          method check_ticker in data_pipeline.py.
                          """)
+
+
+    def filter_tickers(self) -> list:
+        """
+        method to take the initialized list of tickers and checks each individual tickers to make
+        sure that the inputed tickers can be actively traded and validates if fractional trading
+        is enabled based on the strategy requirement. If the tickers cannot be traded, then the
+        list is edited to remove those tickers that cannot be traded. If all tickers cannot be
+        traded, then the list becomes empty and no trades will be executed.
+        """
+        tradability = check_ticker(self.tickers)
+        if self.needs_fractional == False:
+            tradable_tickers = [for ticker in tradability if tradable_tickers.get(ticker).get('tradable') == True]
+        else:
+            tradable_tickers = [for ticker in tradability
+                                 if tradable_tickers.get(ticker).get('tradable') == True
+                                 and tradable_tickers.get(ticker).get('fractional_tradability') == True]
+        return tradable_tickers
+
+
+    def select_tickers(self) -> None:
+        """
+        Checks and sets the tickers according to if they are tradable and whether it fits with the
+        specified strategy when it comes to fractional shares.
+        """
+        logging.info("Checking if desired tickers are tradable...")
+        new_tickers = filter_tickers()
+        logging.info("Setting new tickers...")
+        self.set_tickers(tickers=new_tickers)
+
